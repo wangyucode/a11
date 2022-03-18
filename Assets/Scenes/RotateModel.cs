b4735c831ac5225a63840fecc118cc6cecef76b0
yu.wang <wangyu@wycode.cn>
2022-03-18 23:37:07 +0800

    add depth

--- Assets/Scenes/RotateModel.cs
+++ Assets/Scenes/RotateModel.cs
@@ -5,6 +5,9 @@ public class RotateModel : MonoBehaviour
 {
     float rotationSpeed = Screen.width * 0.0125f;
 
+    public float xMat = 0.03f;
+    public float yMat = 0.02f;
+
     private int lastAction = 0;
 
     private Vector3 deltaVector = Vector3.zero;
@@ -16,57 +19,20 @@ public class RotateModel : MonoBehaviour
     public Material mat2;
     public Material mat3;
     public Material mat4;
-
     public Transform DNATrans;
-    public Transform text1;
-    public Transform text2;
-    public Transform text3;
-    public Transform text4;
-    public Transform text5;
-    public Transform t1Top;
-    public Transform t1DNA;
-    public Transform t2Top;
-    public Transform t2DNA;
-    public Transform t3Top;
-    public Transform t3DNA;
-    public Transform t4Top;
-    public Transform t4DNA;
-    public Transform t5Top;
-    public Transform t5DNA;
-
-    public LineRenderer line1;
-    public LineRenderer line2;
-    public LineRenderer line3;
-    public LineRenderer line4;
-    public LineRenderer line5;
 
     [DllImport("__Internal")]
     private static extern void ShowLink(string link);
 
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(t1Top.position, t1DNA.position);
-        Gizmos.DrawLine(t2Top.position, t2DNA.position);
-        Gizmos.DrawLine(t3Top.position, t3DNA.position);
-        Gizmos.DrawLine(t4Top.position, t4DNA.position);
-        Gizmos.DrawLine(t5Top.position, t5DNA.position);
-    }
-
     void Update()
     {
-        material.mainTextureOffset = new Vector2(Time.time * 0.005f, Time.time * 0.009f);
+        material.mainTextureOffset = new Vector2(Time.time * xMat, Time.time * yMat);
         mat1.mainTextureOffset = new Vector2(Time.time * 0.005f, Time.time * 0.009f);
         mat2.mainTextureOffset = new Vector2(Time.time * 0.005f, Time.time * 0.009f);
         mat3.mainTextureOffset = new Vector2(Time.time * 0.005f, Time.time * 0.009f);
         mat4.mainTextureOffset = new Vector2(Time.time * 0.005f, Time.time * 0.009f);
 
-        if (lastAction == 0) transform.Rotate(Vector3.up * Time.deltaTime * 8f, Space.Self);
-
-        line1.SetPositions(new Vector3[] { t1Top.position, t1DNA.position });
-        line2.SetPositions(new Vector3[] { t2Top.position, t2DNA.position });
-        line3.SetPositions(new Vector3[] { t3Top.position, t3DNA.position });
-        line4.SetPositions(new Vector3[] { t4Top.position, t4DNA.position });
-        line5.SetPositions(new Vector3[] { t5Top.position, t5DNA.position });
+        if (lastAction == 0) DNATrans.Rotate(Vector3.up * Time.deltaTime * 8f, Space.Self);
 
         if (Input.touches.Length == 1)
         {
@@ -80,28 +46,21 @@ public class RotateModel : MonoBehaviour
                     if (lastAction == 0 || lastAction == 1)
                     {
                         deltaVector = touch.deltaPosition;
-                        Vector3 rotateVector = new Vector3(0, deltaVector.x, 0) * Time.deltaTime * rotationSpeed;
-                        transform.Rotate(rotateVector, Space.Self);
+                        DNATrans.Rotate(Vector3.up * deltaVector.x * Time.deltaTime * rotationSpeed, Space.Self);
                         lastAction = 1;
                     }
                     break;
                 case TouchPhase.Ended:
                     lastAction = 0;
-                    float distance = Vector2.Distance(touch.position, startPos);
-                    if (distance < 10)
-                    {
-                        Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                        RaycastHit hit;
-                        if (Physics.Raycast(ray, out hit))
-                        {
-                            ShowLink(hit.collider.gameObject.name);
-                        }
-                    }
                     break;
             }
 
         }
 
+    }
+
+    public void Clicked(string name){
+        ShowLink(name);
     }
 
 }
---
+++ Assets/WebGLTemplates/template1/main.js
@@ -0,0 +1,143 @@
+var canvas = document.querySelector("#unity-canvas");
+var loading = document.querySelector("#index-loading");
+var returnBtn = document.querySelector("#return-btn");
+var imageContainer = document.querySelector("#images-container");
+var landingPage = document.querySelector("#landing-page")
+var sound = document.querySelector("#sound")
+
+window.landingData = {
+    pepper: [
+        { tag: 'img', url: 'landing/pepper/1.jpg' },
+        { tag: 'img', url: 'landing/pepper/2.jpg' },
+        { tag: 'img', url: 'landing/pepper/3.jpg' },
+        { tag: 'img', url: 'landing/pepper/4.jpg' },
+        { tag: 'img', url: 'landing/pepper/5.jpg' },
+        { tag: 'img', url: 'landing/pepper/6.jpg' },
+        { tag: 'img', url: 'landing/pepper/7.jpg' },
+    ],
+    vial: [
+        { tag: 'img', url: 'landing/vial/1.jpg' },
+        { tag: 'img', url: 'landing/vial/2.jpg' },
+        { tag: 'img', url: 'landing/vial/3.jpg' },
+        { tag: 'img', url: 'landing/vial/4.jpg' },
+        { tag: 'img', url: 'landing/vial/5.jpg' },
+        { tag: 'img', url: 'landing/vial/6.jpg' },
+        { tag: 'img', url: 'landing/vial/7.jpg' },
+        { tag: 'img', url: 'landing/vial/8.jpg' },
+        { tag: 'img', url: 'landing/vial/9.jpg' },
+    ],
+    EX: [
+        { tag: 'img', url: 'landing/ex/1.jpg' },
+        { tag: 'img', url: 'landing/ex/2.jpg' },
+        { tag: 'img', url: 'landing/ex/3.jpg' },
+        { tag: 'img', url: 'landing/ex/4.jpg' },
+        { tag: 'img', url: 'landing/ex/5.jpg' },
+        { tag: 'img', url: 'landing/ex/6.jpg' },
+        { tag: 'img', url: 'landing/ex/7.jpg' },
+        { tag: 'img', url: 'landing/ex/8.jpg' },
+        { tag: 'img', url: 'landing/ex/9.jpg' },
+        { tag: 'img', url: 'landing/ex/10.jpg' },
+        { tag: 'img', url: 'landing/ex/11.jpg' },
+    ],
+    TT: [
+        { tag: 'img', url: 'landing/tt/1.jpg' },
+        { tag: 'img', url: 'landing/tt/2.jpg' },
+        { tag: 'img', url: 'landing/tt/3.jpg' },
+        { tag: 'img', url: 'landing/tt/4.jpg' },
+        { tag: 'img', url: 'landing/tt/5.jpg' },
+        { tag: 'img', url: 'landing/tt/6.jpg' },
+    ],
+    jin: [
+        { tag: 'img', url: 'landing/jin/1.jpg' },
+        { tag: 'img', url: 'landing/jin/2.jpg' },
+        { tag: 'img', url: 'landing/jin/3.jpg' },
+        { tag: 'img', url: 'landing/jin/4.jpg' },
+        { tag: 'img', url: 'landing/jin/5.jpg' },
+        { tag: 'img', url: 'landing/jin/6.jpg' },
+        { tag: 'img', url: 'landing/jin/7.jpg' },
+        { tag: 'img', url: 'landing/jin/8.jpg' },
+        { tag: 'img', url: 'landing/jin/9.jpg' },
+    ],
+    a11: [
+        { tag: 'img', url: 'landing/a11/1.jpg' },
+        { tag: 'img', url: 'landing/a11/2.jpg' },
+        { tag: 'img', url: 'landing/a11/3.jpg' },
+    ]
+}
+
+returnBtn.addEventListener('click', function () {
+    landingPage.style.display = 'none';
+    while (imageContainer.lastElementChild) {
+        imageContainer.removeChild(imageContainer.lastElementChild);
+    }
+});
+
+var playSound = function () {
+    sound.load();
+    window.removeEventListener('touchend', playSound)
+}
+
+sound.addEventListener('canplay', function () {
+    sound.play();
+});
+
+window.addEventListener('touchend', playSound);
+
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/{{{ LOADER_FILENAME }}}";
+var config = {
+    dataUrl: buildUrl + "/{{{ DATA_FILENAME }}}",
+    frameworkUrl: buildUrl + "/{{{ FRAMEWORK_FILENAME }}}",
+    codeUrl: buildUrl + "/{{{ CODE_FILENAME }}}",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "wycode",
+    productName: "a11",
+    productVersion: "0.1"
+};
+
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
+
+//   if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+//     // Mobile device style: fill the whole browser client area with the game canvas:
+
+//     var meta = document.createElement('meta');
+//     meta.name = 'viewport';
+//     meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+//     document.getElementsByTagName('head')[0].appendChild(meta);
+//     container.className = "unity-mobile";
+
+//     // To lower canvas resolution on mobile devices to gain some
+//     // performance, uncomment the following line:
+//     // config.devicePixelRatio = 1;
+
+//     canvas.style.width = window.innerWidth + 'px';
+//     canvas.style.height = window.innerHeight + 'px';
+
+//     unityShowBanner('WebGL builds are not supported on mobile devices.');
+//   } else {
+//     // Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
+
+//     canvas.style.width = "960px";
+//     canvas.style.height = "600px";
+//   }
+
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+        //   progressBarFull.style.width = 100 * progress + "%";
+        console.log(progress);
+    }).then((unityInstance) => {
+        setTimeout(() => {
+            loading.style.display = "none";
+        }, 2000);
+    }).catch((message) => {
+        alert(message);
+    });
+}
+document.body.appendChild(script);
